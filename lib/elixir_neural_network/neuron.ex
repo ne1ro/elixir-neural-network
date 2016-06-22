d3edcd1a6c2aa1c31ded659c377a1b41a4ced60f
neiro <neiro.mail@gmail.com>
2016-06-22 22:36:46 +0300

    Add sigmoid function to neuron

--- lib/elixir_neural_network/neuron.ex
+++ lib/elixir_neural_network/neuron.ex
@@ -3,5 +3,19 @@ defmodule ElixirNeuralNetwork.Neuron do
     Single neuron struct
   """
 
-  defstruct input: 0, output: 0, incoming: [], outgoing: [], pid: nil
+  alias NeuralNetwork.{Neuron, Connection}
+
+  defstruct input: 0, output: 0, bias: 0, incoming: [], outgoing: [], pid: nil
+
+  def start_link(fields) do
+    {:ok, pid} = Agent.start_link fn -> %Neuron{} end
+    {:ok, pid}
+  end
+
+  def activate(pid) do
+  end
+
+  defp sigmoid(param) do
+    1 / (1 + :math.exp -param)
+  end
 end
